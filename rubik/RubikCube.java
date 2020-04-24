cde214bf06cff6d813282e69ebb35317e4429d16
Chiranjeev Kumar <chikumar@amazon.com>
2020-04-23 19:14:14 -0700

    Testing solve() method

--- rubik/RubikCube.java
+++ rubik/RubikCube.java
@@ -5,18 +5,18 @@ public class RubikCube {
         System.out.println("Starting...");
         Cube cube = new Cube();
         //cube.rotateCubeLeft();cube.rotateCubeLeft();
-        System.out.println("solved daisy? " + cube.isDaisy());
-        //cube.randomize();
-        //cube.makeDaisy();cube.makeWhiteCross();cube.printDown();
-        //cube.printCube();
-        //System.out.println("solved face? " + cube.solvedRight());
+        //System.out.println("solved isCubeSolved? " + cube.isCubeSolved());
+        cube.randomize();
+//        cube.makeDaisy();cube.makeWhiteCross();
+//        cube.solveBottomLayer();
+//        cube.solveMiddleLayer();
+//        cube.makeYellowCross();
+//        cube.makeYellowFace();
+//        cube.positionTopCorners();
+//        cube.finalStep();
+        cube.solve();
+        cube.printCube();
         //System.out.println("Number of sticker is: " + cube.countSticker());
-//        cube.printFace();
-//        cube.printLeft();
-//        cube.printBack();
-//        cube.printRight();
-//        cube.printUp();
-//        cube.printDown();
     }
 
 }
